2f0653018ac28871744811d2ce18dcf66f3ce232
Michelle Tran <m.tran1223@gmail.com>
2020-10-23 16:28:56 -0700

    first commit

---
+++ src/build/precache-manifest.26453c63b3f754bbffe36b573d683e11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3482d41fb1a89c3e4fa0932cc16c316",
+    "url": "/index.html"
+  },
+  {
+    "revision": "470ff42a13bc5e6aceef",
+    "url": "/static/css/main.f20ee3e2.chunk.css"
+  },
+  {
+    "revision": "9742e2770c6ad32659e9",
+    "url": "/static/js/2.3eb94ed0.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/static/js/2.3eb94ed0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "470ff42a13bc5e6aceef",
+    "url": "/static/js/main.74cac24e.chunk.js"
+  },
+  {
+    "revision": "bbd9ed7b9b5ef41b612c",
+    "url": "/static/js/runtime-main.e47264c4.js"
+  }
+]);
